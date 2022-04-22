688c6eeb59688dae0ab4a10c94f41f3e2c68a988
cicerohr <cicerohr@gmail.com>
2022-04-22 10:02:04 -0300

    More readable.

--- logs/loguru_conf.py
+++ logs/loguru_conf.py
@@ -18,8 +18,6 @@ logger.add(
     rotation='1 day',
     retention='10 days',
     format='{time:YYYY-MM-DD HH:mm:ss} | '
-    '{level} | {message} | '
-    '{file} '
-    '-> {line}',
+    '{level} | {message} | {file} -> {line}',
     # filter=lambda rec: 'api_domain' not in rec['message'].lower(),
 )
