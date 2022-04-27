e3e2f888b0a08b3bbc61a4fcbdfeb7bee2e1c102
cicerohr <cicerohr@gmail.com>
2022-04-27 18:32:16 -0300

    Organizing project.

---
+++ src/challenges/python_pro/12_classe_bola.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+r"""12_classe_bola.py in: 2022-04-27.
+
+Python version: 3.10.0
+
+Classe Bola: Crie uma classe que modele uma bola:
+    - A Atributos: Cor, circunferência, material
+    - B Métodos: trocaCor e mostraCor
+"""
+from tests.loguru_conf import logger
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    logger.info('Início do programa.')
+    main()
+    logger.info('Fim do programa.')
