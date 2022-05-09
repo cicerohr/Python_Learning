a0cfecca17005d11d52df2425ac863cf7ea9c418
cicerohr <cicerohr@gmail.com>
2022-05-08 22:19:45 -0300

    [Refactor]

--- src/minimax.py
+++ src/minimax.py
@@ -2,20 +2,14 @@
 r"""minimax.py in: 2022-05-08.
 
 Python version: 3.10.0
-
-
 """
 from math import log
 
 from tests.loguru_conf import logger
 
 
-def minimax(cur_depth,
-            node_index,
-            max_turn,
-            scores,
-            target_depth
-            ):
+def minimax(cur_depth, node_index, max_turn, scores, target_depth):
+    """Função de minimax."""
     # base case : target_depth reached
     if cur_depth == target_depth:
         return scores[node_index]
@@ -27,17 +21,12 @@ def minimax(cur_depth,
             ),
             minimax(
                 cur_depth + 1, node_index * 2 + 1, False, scores, target_depth
-            )
-        )
-    else:
-        return min(
-            minimax(
-                cur_depth + 1, node_index * 2, True, scores, target_depth
             ),
-            minimax(
-                cur_depth + 1, node_index * 2 + 1, True, scores, target_depth
-            )
         )
+    return min(
+        minimax(cur_depth + 1, node_index * 2, True, scores, target_depth),
+        minimax(cur_depth + 1, node_index * 2 + 1, True, scores, target_depth),
+    )
 
 
 def main():
@@ -46,7 +35,7 @@ def main():
 
     tree_depth = log(len(scores), 2)
 
-    print("The optimal value is : ", end="")
+    print('The optimal value is : ', end='')
     print(minimax(0, 0, True, scores, tree_depth))
 
 
